430e9881173c49ee06e04487a6ba496c21ecab09
kateheese <kateheese@gmail.com>
2015-12-08 14:42:10 -0700

    added seed data and URL to README.md

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 API for Colorado 14ers including:
-Peak Name
-Range
-Rank
-Elevation
-Latitude and Longitude
-Towns
-National Forests
+- Peak Name
+- Range
+- Rank
+- Elevation
+- Latitude and Longitude
+- Towns
+- National Forests
+
+https://colorado-14ers-api.herokuapp.com/
---
+++ migrations/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+  peaks
+VALUES
+  ( default, 'Mt. Elbert', 'Sawatch Range', 1, 14433, '39 07 04 N', '106 26 41 W' ),
+  ( default, 'Mt. Massive', 'Sawatch Range', 2, 14421, '39 11 14 N', '106 28 29 W' ),
+  ( default, 'Mt. Harvard', 'Sawatch Range', 3, 14420, '38 55 28 N', '106 19 12 W' ),
+  ( default, 'Blanca Peak', 'Sangre de Cristo', 4, 14345, '37 34 38 N', '105 29 07 W' ),
+  ( default, 'La Plata Peak', 'Sawatch Range', 5, 14336, '39 01 46 N', '106 28 21 W' ),
+  ( default, 'Uncompahgre Peak', 'San Juan Mountains', 6, 14309, '38 04 18 N', '107 27 41 W' ),
+  ( default, 'Crestone Peak', 'Sangre de Cristo', 7, 14294, '37 58 00 N', '105 35 05 W' ),
+  ( default, 'Mt. Lincoln', 'Mosquito Range', 8, 14286, '39 21 05 N', '106 06 39 W' ),
+  ( default, 'Grays Peak', 'Front Range', 9, 14270, '39 38 02 N', '105 49 01 W' );
